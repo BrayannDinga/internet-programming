56a4d5ba9832d037281df2ce09591cf724c5577a
ASONG JUNIOR <57803773+ajunior9@users.noreply.github.com>
2023-11-09 17:16:59 +0100

    Add files via upload
    Update of Admin files

---
+++ Attendance Management system/Admin/Includes/footer.php
@@ -0,0 +1,9 @@
+ <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span> &copy; <script> </script> - Developed by Asong Junior, Brayann Dinga, Faith Nazah, Ngong Melissa
+              STUDENT ATTENDANCE MANAGEMENT SYSTEM
+            </span>
+          </div>
+        </div>
+      </footer>
